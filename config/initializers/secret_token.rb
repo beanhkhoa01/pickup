bce87a8aa0ffe1a24a3dd3a2aa2c48ee74503af7
Demitri Tzitzon <dtzitzon@wearableintelligence.com>
2015-07-27 10:38:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pickup::Application.config.secret_key_base = 'e80331738078e8d8c08bc50c3b308568fc25917bf8979067d77f8163e5cf2b4fb8936f7979702fa4a9f145f22996938ffa8fd33041674a9fcfeae1d9b33225fb'
